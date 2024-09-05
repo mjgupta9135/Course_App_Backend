1d4dd818fcbefbeb0a84ce781a49528463419fc4
Mrityunjay <mjguptacse@gmail.com>
2024-09-05 20:56:40 +0530

    updated price parameter to number

--- db/index.js
+++ db/index.js
@@ -23,7 +23,7 @@ const UserSchema = new mongoose.Schema({
 const CourseSchema = new mongoose.Schema({
   title: String,
   description: String,
-  price: String,
+  price: Number,
   imageLink: String,
 });
 
